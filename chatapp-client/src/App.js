c4d71cf6747ac2e4812570780373beceed5e30cb
leoyakubov <leo.yakubov@gmail.com>
2024-03-08 18:47:03 +0700

    implemented chat client

--- chatapp-client/src/App.js
+++ chatapp-client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Signin from "./signin/Signin";
+import Signup from "./signup/Signup";
+import Profile from "./profile/Profile";
+import Chat from "./chat/Chat";
+import "./App.css";
+
+export const AppContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+
+          <Route path="/login" element={<Signin />} />
+
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
